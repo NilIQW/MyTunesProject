e5e3e6370da4e38a8c61da3f9c87ac183c29ea0a
salmacx <145067369+salmacx@users.noreply.github.com>
2023-12-04 13:34:21 +0100

    updated song class

--- src/be/Song.java
+++ src/be/Song.java
@@ -1,18 +1,28 @@
 package be;
 
 public class Song {
-    private int ID = -1;
+    private int id = -1;
     private String name;
     private String artist;
     private String filePath;
 
 
-    public int getID() {
-        return ID;
+    public Song (int id , String name, String artist, String filePath) {
+        this.id= id;
+        this.name= name;
+        this.artist= artist;
+        this.filePath= filePath;
+
     }
 
-    public void setID(int ID) {
-        this.ID = ID;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -30,4 +40,21 @@ public class Song {
     public void setArtist(String artist) {
         this.artist = artist;
     }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+    @Override
+    public String toString (){
+        return "Song=" +
+                "id= " + id +
+                "name= " + name +
+                "artist= " + artist +
+                "file path= " + filePath ;
+    }
+
 }
--- src/gui/controller/MyTunesController.java
+++ src/gui/controller/MyTunesController.java
@@ -1,5 +1,6 @@
 package gui.controller;
 
+import be.Song;
 import gui.view.NewPlaylist;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -9,8 +10,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 import java.net.URL;
@@ -18,10 +21,21 @@ import java.util.ResourceBundle;
 
 
 public class MyTunesController implements Initializable {
+
+    public Song song;
+
     @FXML
     public ListView<String> songListView;
+    public Button newPlaylist;
+    public Button editPlaylist;
+    public Button deletePlaylist;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        song = new Song(1, "song1", "shehshe", "media/BEAUZ&MOMO-Won'tLookBack[NCS Release].mp3");
+        song.toString();
+        System.out.println(song);
+
         ObservableList<String> songs = FXCollections.observableArrayList(
                 "");
         MyTunesController myTunesController = new MyTunesController();
